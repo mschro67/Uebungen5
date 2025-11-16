d8a4c19b8ead3c09d7ca5efa4b60877afc98206c
maximilian107 <maximilian.s107@evt.schule.koeln>
2025-11-16 09:32:18 +0100

    Add H2-H3 (incomplete)

--- src/h2/h2_main.java
+++ src/h2/h2_main.java
@@ -9,23 +9,24 @@ public class h2_main{
 
         digits=0;
         int z=n;
-        int b;
 
         while (z>0) {
             z /= 10;
             digits += 1;
         }
 
-        for (int x=1;x<=digits;x++) {
+        System.out.println("digits: "+digits);
+
+        for (int x=0;x<digits;x++) {
             System.out.println("x: " + x);
             z=n;
-            for (int y = 1; y <= x; y++) {
-                b=z/10;
-                z=z-b*10;
-                System.out.println("z: "+z);
+            for (int y=0;y<x;y++){
+                z/=10;
+                System.out.println("! y: "+y+", z: "+z);
             }
+            for (int y=x;y>0;y--)
+            a[8-x]=z;
         }
-
-        System.out.println("digits: "+digits);
+        System.out.println("a: "+a[0]+","+a[1]+","+a[2]+","+a[3]+","+a[4]+","+a[5]+","+a[6]+","+a[7]+","+a[8]);
     }
 }
--- src/h3/h3_main.java
+++ src/h3/h3_main.java
@@ -4,6 +4,13 @@ package h3;
 
 public class h3_main{
     public static void main(String[] args){
-        System.out.println("Nothing");
+        int[][] einheiten=new int[2][15];
+
+        int[] x={50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        einheiten[0]=x;
+
+        for (int y=0;y<einheiten[0].length;y++){
+            einheiten[1][y]=einheiten[0][y]/100;
+        }
     }
 }
